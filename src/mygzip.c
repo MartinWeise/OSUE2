2b2427d763a23b937af718adafa528b6441ff1cc
Martin Weise <martin.weise@gmx.at>
2016-11-23 01:26:14 +0100

    pipes ok, gzip not working

--- src/mygzip.c
+++ src/mygzip.c
@@ -15,6 +15,7 @@
 #include <errno.h>
 #include <memory.h>
 #include <sys/wait.h>
+#include <stdarg.h>
 
 /*
  *  ---------                ---------
@@ -31,11 +32,21 @@
  *        .___________________\___/
  */
 
- void error_exit (char *message, ...);
+char *progname;
 
- void usage(char **argv);
+#define MESSAGE "hello world"
 
- void parse_args(int argc, char **argv, char **filename);
+/* === Prototypes === */
+
+/**
+ * @copyright OS UE Team (server.c)
+ * @param fmt
+ */
+void error_exit (const char *fmt, ...);
+
+void usage();
+
+void parse_args(int argc, char **argv, char **filename);
 
 /**
  * Program entry point.
@@ -47,13 +58,12 @@
  * @return EXIT_SUCCESS | EXITFAILURE when operated like required.
  */
 int main(int argc, char **argv) {
-    char *filename;
-    pid_t pid1, pid2;
+    char *filename = NULL;
+    FILE *file;
+    pid_t pid;
     int pipe1[2], pipe2[2];
-    int status1;
     //        ^ 0 = read end
     //          1 = write end
-    FILE *file, *fdpipe;
 
     parse_args(argc, argv, &filename);
 
@@ -61,59 +71,98 @@ int main(int argc, char **argv) {
         error_exit ("Can't create pipes.");
     }
 
-    switch (pid1 = fork()) {
-        case -1:
-            error_exit ("Cannot fork!");
-        case 0:
-            // child 1
-            printf ("child 1.\n");
-            /* close write end */
-            exit (EXIT_SUCCESS);
-            break;
-        default:
-            // parent
-            switch (pid2 = fork()) {
-                case -1:
-                    error_exit ("Cannot fork!");
-                case 0:
+    for (int i = 0; i < 2; i++) {
+        printf("Child: %d\n", i+1);
+        switch (pid = fork()) {
+            case -1:
+                error_exit("Cannot fork!");
+            case 0:
+                if (i == 0) {
+                    // child
+                    close (pipe1[0]);
+                    if (dup2 (pipe1[1], STDIN_FILENO) < 0) {
+                        error_exit ("Could not apply stdin to pipe1.");
+                    }
+//                    write (pipe1[1], "hello", sizeof("hello")+1);
+                    close (pipe2[1]);
+                    if (dup2 (pipe2[0], STDOUT_FILENO) < 0) {
+                        error_exit ("Could not apply stdout to pipe2.");
+                    }
+                    /* stdout is closed indirect */
+                    execlp("gzip", "gzip", "-cf", (char *)NULL);
+//                    exit (EXIT_SUCCESS);
+                } else {
                     // child 2
-                    printf ("child 2.\n");
+                    close (pipe1[0]);
+                    close (pipe1[1]);
+                    close (pipe2[1]);
+                    char buffer[1024];
+
+                    if (filename == NULL) {
+                        file = stdout;
+                        fprintf(file, "--->output\n");
+                    } else {
+                        // append binary
+                        file = fopen(filename, "ab");
+                        while (read(pipe2[0], buffer, sizeof buffer) < 0) {
+                            fprintf (file, "%s", buffer);
+                        }
+                    }
                     exit (EXIT_SUCCESS);
-                    break;
-                default:
-                    // still parent
-                    printf ("Parent 2.\n");
-                    break;
-            }
-            // Parent
-            printf("Parent.\n");
-            if (wait(&status1) == -1) {
-                error_exit ("Couldn't wait.");
-            }
-            break;
+                }
+                exit(EXIT_SUCCESS);
+                break;
+            default:
+                // parent
+                if (i == 0) {
+                    // child 1
+                    // note: close (pipe1[0]) = read end would affect childs!
+                    char buffer[1024];
+                    /* read data from stdin */
+                    while(fgets(buffer, sizeof buffer, stdin) != NULL) {
+                        fprintf(stdout, "Read: %s", buffer);
+                        write(pipe1[1], buffer, strlen(buffer)+1);
+                    }
+                    close (pipe1[1]);
+                    /* write data to first pipe (gzip) */
+                } else {
+                    // from child 2
+                    close (pipe2[0]);
+                    close (pipe2[1]);
+                }
+                wait(NULL); // important
+        }
     }
 
     return EXIT_SUCCESS;
 }
 
-void parse_args(int argc, char **argv, char **filename) {
+void parse_args (int argc, char **argv, char **filename) {
     if (argc > 2) {
-        usage(argv);
+        usage();
     }
-    printf("OK\n");
+    progname = argv[0];
+    if (argc == 2) {
+        *filename = argv[1];
+    }
 }
 
-void error_exit (char *message, ...) {
-    fprintf (stderr, "%s\n", message);
-    exit (EXIT_FAILURE);
+void error_exit (const char *fmt, ...) {
+    va_list ap;
+
+    (void) fprintf(stderr, "%s: ", progname);
+    if (fmt != NULL) {
+        va_start(ap, fmt);
+        (void) vfprintf(stderr, fmt, ap);
+        va_end(ap);
+    }
+    if (errno != 0) {
+        (void) fprintf(stderr, ": %s", strerror(errno));
+    }
+    (void) fprintf(stderr, "\n");
 }
 
-/**
- * useage information
- * @brief This function writes helpful usage information about the program to stderr and exites then.
- * @details global variables: argv
- */
-void usage(char **argv) {
-    fprintf (stderr, "USAGE: %s [file]\n", argv[0]);
+void usage() {
+    fprintf (stderr, "USAGE: %s [file]\n", progname);
     exit (EXIT_FAILURE);
 }
